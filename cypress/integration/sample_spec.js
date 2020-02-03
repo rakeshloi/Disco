a2947f721298df5265ee3eae767fcf327e0fbabf
rakeshloi <rakeshloi@users.noreply.github.com>
2020-02-03 16:56:55 +0000

    added percy and recording

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,15 +1,31 @@
+import '@percy/cypress';
+
 describe('My First Test', function() {
     it('Gets, types and asserts', function() {
-      cy.visit('https://example.cypress.io')
+      cy.visit('/login?flow=login')
   
-      cy.contains('type').click()
+      //cy.contains('SIGN IN').click()
   
       // Should be on a new URL which includes '/commands/actions'
-      cy.url().should('include', '/commands/actions')
+      //cy.url().should('include', '/commands/actions')
   
       // Get an input, type into it and verify that the value has been updated
-      cy.get('.action-email')
+      cy.get('input[name="email"]')
         .type('fake@email.com')
-        .should('have.value', 'fake@email.com')
+
+      cy.get('input[name="password"]')
+        .type('password')
+        //.should('have.value', 'fake@email.com')
+
+      cy.get('button[type="submit"]').click()
+
+      // Take a snapshot for visual diffing
+    cy.percySnapshot("one")
+
+      cy.get('button[class="cookie-policy__accept-button-container--button__3KyZP"]').click()
+
+      // Take a snapshot for visual diffing
+    cy.percySnapshot("two");
+
     })
   })
--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -15,3 +15,10 @@ module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
 }
+
+// In cypress/plugins/index.js
+let percyHealthCheck = require('@percy/cypress/task')
+
+module.exports = (on, config) => {
+  on("task", percyHealthCheck);
+};
